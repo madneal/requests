c10282ce7e2381090e540233b8d9185de288b998
neal1991 <neal1991@sina.com>
2020-01-13 13:16:58 +0800

    modify the length of method :card_file_box:

--- models.go
+++ models.go
@@ -11,7 +11,7 @@ import _ "github.com/jinzhu/gorm/dialects/mysql"
 type Asset struct {
 	Id     int64  `gorm:"type:bigint(20) auto_increment;column:id;primary_key"`
 	Url    string `gorm:"type:varchar(300);column:url"`
-	Method string `gorm:"type:varchar(5);column:method"`
+	Method string `gorm:"type:varchar(10);column:method"`
 	Md5    string `gorm:"type:varchar(100);column:md5"`
 }
 
@@ -19,7 +19,7 @@ type Resource struct {
 	Id        int64  `gorm:"type:bigint(20) auto_increment;column:id;primary_key"`
 	Url       string `gorm:"type:varchar(300);column:url"`
 	Protocol  string `gorm:"type:varchar(10);column:protocol"`
-	Method    string `gorm:"type:varchar(5);column:method"`
+	Method    string `gorm:"type:varchar(10);column:method"`
 	Firstpath string `gorm:"type:varchar(100);column:firstpath"`
 	Ip        string `gorm:"type:varchar(20);column:ip"`
 }

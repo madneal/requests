76dd10c38d115d836c0f073598f7f3ef86f016e4
neal1991 <neal1991@sina.com>
2020-05-22 17:45:28 +0800

    output different levels log to different writer :loud_sound:

--- log.go
+++ log.go
@@ -3,12 +3,31 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
 var Log *logrus.Entry
 
 const LOG_FILE = "pvs.log"
+
+type WriterHook struct {
+	Writer    io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *WriterHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	_, err = hook.Writer.Write([]byte(line))
+	return err
+}
+
+func (hook *WriterHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
 
 func init() {
 	logger := logrus.New()
@@ -21,8 +40,18 @@ func init() {
 		logger.Fatal(err)
 	}
 	//defer file.Close()
-	logger.SetReportCaller(true)
-	mw := io.MultiWriter(os.Stdout, file)
-	logger.SetOutput(mw)
+	//logger.SetReportCaller(true)
+	//mw := io.MultiWriter(os.Stdout, file)
+	//logger.SetOutput(mw)
+	fileWriter := io.Writer(file)
+	logger.SetOutput(ioutil.Discard)
+	logger.AddHook(&WriterHook{
+		Writer:    fileWriter,
+		LogLevels: []logrus.Level{logrus.PanicLevel, logrus.FatalLevel, logrus.ErrorLevel, logrus.WarnLevel},
+	})
+	logger.AddHook(&WriterHook{
+		Writer:    os.Stdout,
+		LogLevels: []logrus.Level{logrus.InfoLevel, logrus.DebugLevel},
+	})
 	Log = logger.WithFields(logrus.Fields{"prefix": "pvs"})
 }
--- log_test.go
+++ log_test.go
@@ -9,4 +9,5 @@ func TestLog(t *testing.T) {
 	Log = Log.WithFields(logrus.Fields{"url": "adfasd412384198jj", "ip": "1.1.1.1"})
 	Log.Info("test1234")
 	Log.Info("afasdfqwerqw")
+	Log.Warn("warn warn warn")
 }

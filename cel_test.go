29621f3b2f04e45daac85a8630c2cb219e803af0
neal1991 <neal1991@sina.com>
2020-07-27 13:43:02 +0800

    add cel check :rocket:

---
+++ cel_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCheck(t *testing.T) {
+	def := make([]InterpretableDefinition, 0)
+	def = append(def,InterpretableDefinition{
+		CheckExpression: "req_method.contains(\"GET\")",
+	})
+	r := Request{
+		Method: "GET",
+	}
+	result, err := Check(def, &r)
+	fmt.Println(err)
+	fmt.Println(result)
+}

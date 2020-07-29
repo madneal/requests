536a828971e962e3d114fd0e5cf55c62d5254732
neal1991 <neal1991@sina.com>
2020-07-29 10:52:23 +0800

    finish the build script

--- build.sh
+++ build.sh
@@ -1,26 +1,32 @@
 # build for linux
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
-#!/bin/bash
-
 function obtain_git_branch {
   br=`git branch | grep "*"`
   echo ${br/* /}
 }
+
 result=`obtain_git_branch`
 echo Current git branch is $result
 if [ "$result" == "master" ]
  then
    echo Building for the branch $result
    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+   echo Finished building process
+   git checkout cel
+   echo Current git branch is `obtain_git_branch`
+   echo Building for the branch `obtain_git_branch`
+   CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cel
+   echo Finished building process
+   git checkout master
+elif [ "result" == "cel" ]
+  then
+   echo Building for the branch $result
+   CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cel
+   echo Finished building process
+   git checkout master
+   echo Current git branch is `obtain_git_branch`
+   echo Building for the branch `obtain_git_branch`
+   CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+   echo Finished building process
 fi
 
-git checkout cel
-result=`obtain_git_branch`
-echo Current git branch is $result
-if [ "$result" == "cel" ]
-  then
-    echo Building for the branch $result
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cel
-    git checkout master
-fi
 

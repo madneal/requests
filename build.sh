cea24296de03f904d6c1c062cdaa93467337fb92
neal1991 <neal1991@sina.com>
2020-07-29 11:06:41 +0800

    finish the build script

--- build.sh
+++ build.sh
@@ -11,18 +11,18 @@ if [ "$result" == "master" ]
    echo Building for the branch $result
    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
    echo Finished building process
-   git checkout cel >&/dev/null
+   git checkout cel
    echo Current git branch is `obtain_git_branch`
    echo Building for the branch `obtain_git_branch`
    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cel
    echo Finished building process
-   git checkout master >&/dev/null
+   git checkout master
 elif [ "result" == "cel" ]
   then
    echo Building for the branch $result
    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cel
    echo Finished building process
-   git checkout master >&/dev/null
+   git checkout master
    echo Current git branch is `obtain_git_branch`
    echo Building for the branch `obtain_git_branch`
    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build

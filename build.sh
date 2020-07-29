49e04637210512b7eaacd924d3e0ec399726629f
neal1991 <neal1991@sina.com>
2020-07-29 10:38:30 +0800

    add branch checkout

--- build.sh
+++ build.sh
@@ -1,2 +1,23 @@
 # build for linux
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+#!/bin/bash
+
+function obtain_git_branch {
+  br=`git branch | grep "*"`
+  echo ${br/* /}
+}
+result=`obtain_git_branch`
+echo Current git branch is $result
+if [ "$result" == "master" ]
+ then
+   CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+fi
+
+git checkout cel
+result=`obtain_git_branch`
+echo Current git branch is $result
+if [ "$result" == "cel" ]
+  then
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o cel
+fi
+

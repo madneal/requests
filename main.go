b2f43219dcbba0fecc7ec28866d9ad40b7d04ab1
neal1991 <neal1991@sina.com>
2020-08-24 14:25:41 +0800

    remove Move resource option :fire:

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"net/url"
 	"os"
-	"time"
 )
 
 func init() {
@@ -43,40 +41,5 @@ func main() {
 		Log.Info(Encrypt(os.Args[2], ENCRYPT_KEY))
 	} else if cmd == "d" {
 		Log.Info(Decrypt(os.Args[2], ENCRYPT_KEY))
-	} else if cmd == "move" {
-		MoveResourcesToAssets()
 	}
-}
-
-func MoveResourcesToAssets() {
-	resources, err := QueryAllServices()
-	if err != nil {
-		Log.Error(err)
-		return
-	}
-	for _, resource := range *resources {
-		urlStr := "http://" + resource.Url
-		u, err := url.Parse(urlStr)
-		if err != nil {
-			Log.Error(err)
-		}
-		ip := GetIpStr(u.Host)
-		if !MatchIp(ip) {
-			continue
-		}
-		asset := Asset{
-			Host:        u.Host,
-			Ip:          ip,
-			Env:         QA_ENV,
-			CreatedTime: time.Now(),
-			UpdatedTime: time.Now(),
-		}
-		err = NewAsset(&asset)
-		if err != nil {
-			Log.Error(err)
-		} else {
-			Log.Infof("Insert Asset successfully! Host: %s; Ip: %s", asset.Host, asset.Ip)
-		}
-	}
-	fmt.Println("Move finished")
 }
